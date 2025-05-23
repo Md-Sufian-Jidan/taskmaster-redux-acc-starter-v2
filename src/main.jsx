82596e28aef453e6812e1575e97033f498631bf0
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-05-23 19:51:16 +0600

    baseapi created

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import { RouterProvider } from 'react-router-dom';
 import routes from './routes/routes.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={routes} />
+      <Toaster
+        position="top-center"
+      />
     </Provider>
   </React.StrictMode>
 );
